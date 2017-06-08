09515d3d979829c8d0a8e5d3db0a4bf16de0efbb
opooc <admin@opooc.com>
2017-06-08 12:42:46 +0800

    Frist

---
+++ iOSClientOfQFNU/iOSClientOfQFNU/Others/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSClientOfQFNU
+//
+//  Created by doushuyao on 17/6/8.
+//  Copyright © 2017年 iOSClientOfQFNU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSClientOfQFNU/iOSClientOfQFNU/Others/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSClientOfQFNU
+//
+//  Created by doushuyao on 17/6/8.
+//  Copyright © 2017年 iOSClientOfQFNU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
