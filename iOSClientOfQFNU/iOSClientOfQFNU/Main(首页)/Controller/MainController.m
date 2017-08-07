0c50301dbebe10e8d0cf08103fa3b049010530ac
chufeng <chufeng@users.noreply.github.com>
2017-08-07 15:35:27 +0800

    删除main主页中不必要代码，让其能正常运行

--- iOSClientOfQFNU/iOSClientOfQFNU/Main(首页)/Controller/MainController.m
+++ iOSClientOfQFNU/iOSClientOfQFNU/Main(首页)/Controller/MainController.m
@@ -204,40 +204,6 @@
 
 
 }
-- (void)leftViewWillLayoutSubviewsWithSize:(CGSize)size {
-    [super leftViewWillLayoutSubviewsWithSize:size];
-    
-    if (!self.isLeftViewStatusBarHidden) {
-        self.leftView.frame = CGRectMake(0.0, 20.0, size.width, size.height-20.0);
-    }
-}
-
-- (void)rightViewWillLayoutSubviewsWithSize:(CGSize)size {
-    [super rightViewWillLayoutSubviewsWithSize:size];
-    
-    if (!self.isRightViewStatusBarHidden ||
-        (self.rightViewAlwaysVisibleOptions & LGSideMenuAlwaysVisibleOnPadLandscape &&
-         UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad &&
-         UIInterfaceOrientationIsLandscape(UIApplication.sharedApplication.statusBarOrientation))) {
-            self.rightView.frame = CGRectMake(0.0, 20.0, size.width, size.height-20.0);
-        }
-}
-
-- (BOOL)isLeftViewStatusBarHidden {
-    if (self.type == 8) {
-        return UIInterfaceOrientationIsLandscape(UIApplication.sharedApplication.statusBarOrientation) && UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone;
-    }
-    
-    return super.isLeftViewStatusBarHidden;
-}
-
-- (BOOL)isRightViewStatusBarHidden {
-    if (self.type == 8) {
-        return UIInterfaceOrientationIsLandscape(UIApplication.sharedApplication.statusBarOrientation) && UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone;
-    }
-    
-    return super.isRightViewStatusBarHidden;
-}
 
 
 - (void)dealloc {
