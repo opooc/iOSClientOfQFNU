ba9ee0210003a7d14fd425e3ba9be251c015da22
chufeng <chufeng@users.noreply.github.com>
2017-09-04 09:59:40 +0800

    添加图片和model

---
+++ iOSClientOfQFNU/UserInfo.h
@@ -0,0 +1,20 @@
+//
+//  UserInfo.h
+//  iOSClientOfQFNU
+//
+//  Created by 易达威 on 2017/9/3.
+//  Copyright © 2017年 iOSClientOfQFNU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserInfo : NSObject
+@property (nonatomic, copy) NSString * user_id;
+@property (nonatomic, copy) NSString *campus;
+@property (nonatomic, copy) NSString *faculty;
+@property (nonatomic, copy) NSString *profession;
+@property (nonatomic, copy) NSString *clazz;
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *portrait;
+
+@end
---
+++ iOSClientOfQFNU/UserInfo.m
@@ -0,0 +1,13 @@
+//
+//  UserInfo.m
+//  iOSClientOfQFNU
+//
+//  Created by 易达威 on 2017/9/3.
+//  Copyright © 2017年 iOSClientOfQFNU. All rights reserved.
+//
+
+#import "UserInfo.h"
+
+@implementation UserInfo
+
+@end
